f12ca5c4e2149cb49586107e860a666260910cb8
RafaCasanova <rafa.casanova.matos@gmail.com>
2022-06-05 18:00:33 -0300

    campos nome e valor não pode ser null ou empty

--- src/main/java/br/com/minhafinanca/controlefinanceiro/controller/form/DespesaForm.java
+++ src/main/java/br/com/minhafinanca/controlefinanceiro/controller/form/DespesaForm.java
@@ -1,17 +1,21 @@
 package br.com.minhafinanca.controlefinanceiro.controller.form;
 
 import br.com.minhafinanca.controlefinanceiro.model.Despesa;
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 @Getter @Setter
 public class DespesaForm {
 
+    @NotNull @NotEmpty
     private String nome;
 
     private String descricao;
 
+    @NotEmpty @NotNull
     private BigDecimal valor;
 
     public Despesa converter(){
