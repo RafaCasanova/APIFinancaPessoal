443d0b7f56115300e1d05d21a8cf337929558004
Rafael Casanova <66842558+RafaCasanova@users.noreply.github.com>
2022-06-09 16:53:08 -0300

    Create Planejamento.java
    planejamento.

---
+++ src/main/java/br/com/minhafinanca/controlefinanceiro/model/Planejamento.java
@@ -0,0 +1,9 @@
+
+public class Planejamento{
+  
+  private String titulo = "Planejamento";
+  private BigDecimal valor = BigDecimal.ZERO;
+  // enum private dataInicial; //EnumMes
+  // enum private dataFinal; //EnumMes
+  
+}
